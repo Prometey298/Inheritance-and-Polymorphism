4f82bdad784da488dbd7fe0c37d1be28c264cc19
Prometey298 <Dimazwww@yandex.ru>
2024-10-31 09:58:25 +0500

    random

--- Enemy.java
+++ Enemy.java
@@ -1,7 +1,10 @@
-public abstract  class Enemy implements Mortal {
-    protected String name;    
+public abstract class Enemy implements Mortal {
+    protected String name;
     protected int health;
-    
+    protected static final int INITIAL_HEALTH = 25; 
+    protected boolean isResurrected = false; 
+    protected static final double RESURRECTION_CHANCE = 0.5; 
+
     public Enemy(int health) {
         this.health = health;
     }
@@ -16,17 +19,26 @@ public abstract  class Enemy implements Mortal {
 
     public abstract void attackHero(Hero hero);
 
-    public synchronized  void  takeDamage(int damage) {
+    public synchronized void takeDamage(int damage) {
         health -= damage;
         if (health < 0) {
-            health = 0; 
-        }
-        System.out.println(name + " получил урон: " + damage + ". Осталось здоровья: " + health);
-        if (health == 0) {
-            if (health == 0) {                     
-            System.out.println(name + " побеждён.");
+            health = 0;
+            System.out.println(name + " получил урон: " + damage + ". Осталось здоровья: " + health);
+            if (!isResurrected && Math.random() < RESURRECTION_CHANCE) {
+                resurrect();
             }
-        }        
+            if (health == 0) {
+                System.out.println(name + " побеждён.");
+            }
+        } else {
+            System.out.println(name + " получил урон: " + damage + ". Осталось здоровья: " + health);
+        }
+    }
+
+    private void resurrect() {
+        health = INITIAL_HEALTH; 
+        isResurrected = true; 
+        System.out.println(name + " воскрес из мёртвых!");
     }
 
     @Override
--- Zombie.java
+++ Zombie.java
@@ -1,18 +1,16 @@
 public class Zombie extends Enemy {
-
-    private static final int INITIAL_HEALTH = 25000;
-    //private String name;
+    private static final int INITIAL_HEALTH = 25;
 
     public Zombie(String name) {
         super(INITIAL_HEALTH);
         this.name = name;
-
     }
+
     @Override
     public void attackHero(Hero hero) {
         if (!isAlive()) {
             return;
-        } 
+        }
         int damage = 10;
         System.out.println(name + " кусает!");
         hero.takeDamage(damage);
